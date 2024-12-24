582e3ac11e4d2de801e233acc3ba671d4404e482
llladno <man30968@gmail.com>
2024-12-24 12:56:51 +0300

    chore: rename bank

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,22 +1,22 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import {ref} from "vue";
 
 export const useFormStore = defineStore('formStore', () => {
   const isOpen = ref(false)
   const formVariant = ref('')
-  const companyInfo = {
+  const  companyInfo = {
     name: 'СПБГЭУ Банк',
-    companyForm: 'ООО',
-    inn: '1234567890',
-    kpp: '123456789',
-    account: '409118100001800000199',
-    bik: '044525225',
-    bank: 'АО "Тинькофф Банк"',
+    companyForm: "ООО",
+    inn: "1234567890",
+    kpp: "123456789",
+    account:'409118100001800000199',
+    bik: "044525225",
+    bank: 'АО "Перевод Банк"',
     corrAccount: '30101810400000000625',
-    address: 'г. Москва, ул. Московская, д. 1',
-    phone: '+7 (123) 456-78-90',
-    email: 'bank@example.com',
-    director: 'Иванов Иван Иванович',
+    address: "г. Москва, ул. Московская, д. 1",
+    phone: "+7 (123) 456-78-90",
+    email: "bank@example.com",
+    director: 'Иванов Иван Иванович'
   }
 
   const openForm = (variant: string) => {
